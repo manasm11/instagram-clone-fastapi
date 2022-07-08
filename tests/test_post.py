02564df573116dca679fc380f137f5f6c8d239fc
Manas Mishra <f20170546@pilani.bits-pilani.ac.in>
2022-07-08 14:53:35 +0000

    Created test template for post create method

--- tests/test_post.py
+++ tests/test_post.py
@@ -1,5 +1,30 @@
-# from .common import TestCase
+from tests.common import TestCase
 
 
-# class TestPost(TestCase):
-#     pass
+class TestPost(TestCase):
+    def test_create_post(self):
+        assert False, "Not implemented"
+
+    def test_create_post__image_url__missing(self):
+        assert False, "Not implemented"
+
+    def test_create_post__image_url__type_missing(self):
+        assert False, "Not implemented"
+
+    def test_create_post__caption__missing(self):
+        assert False, "Not implemented"
+
+    def test_create_post__creator_id__missing(self):
+        assert False, "Not implemented"
+
+    def test_create_post__image_url__incorrect_format(self):
+        assert False, "Not implemented"
+
+    def test_create_post__image_url_type__incorrect(self):
+        assert False, "Not implemented"
+
+    def test_create_post__creator_id__doesnt_exists(self):
+        assert False, "Not implemented"
+
+    def test_create_post__creator_id__incorrect_datatype(self):
+        assert False, "Not implemented"
